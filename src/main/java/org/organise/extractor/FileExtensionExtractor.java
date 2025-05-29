350497c96e6827e7cde455aba2df1c87c2e82871
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-05-29 12:48:27 +0200

    Adding code for extracting the file extension from the file name.

--- src/main/java/org/organise/extractor/FileExtensionExtractor.java
+++ src/main/java/org/organise/extractor/FileExtensionExtractor.java
@@ -1,19 +1,106 @@
 package org.organise.extractor;
 
-import java.io.IOException;
+import java.io.File;
 import java.nio.file.Path;
-import java.nio.file.Files;
 
-import static java.nio.file.Files.probeContentType;
 
 public class FileExtensionExtractor implements DataExtractor<String> {
+    private static final int NOT_FOUND = -1;
+    /**
+     * The extension separator character.
+     */
+    public static final char EXTENSION_SEPARATOR = '.';
+    /**
+     * The Unix separator character.
+     */
+    private static final char UNIX_NAME_SEPARATOR = '/';
+
+    /**
+     * The Windows separator character.
+     */
+    private static final char WINDOWS_NAME_SEPARATOR = '\\';
+    /**
+     * The system separator character.
+     */
+    private static final char SYSTEM_NAME_SEPARATOR = File.separatorChar;
+    private static final char OTHER_SEPARATOR = flipSeparator(SYSTEM_NAME_SEPARATOR);
+    private static final String EMPTY_STRING = "";
+
+
     @Override
     public String extract(Path file) {
-        return null;
+        return getExtension(file.getFileName().toString());
     }
 
-    public String simpleExtensionExtract(Path file) throws IOException {
-        String contentType = probeContentType(file);
-        return contentType != null ? contentType : "No extension found";
+    public static String getExtension(final String fileName) throws IllegalArgumentException {
+        if (fileName == null) {
+            return null;
+        }
+        final int index = indexOfExtension(fileName);
+        if (index == NOT_FOUND) {
+            return EMPTY_STRING;
+        }
+        return fileName.substring(index + 1);
+    }
+
+    private static int indexOfExtension(final String fileName) throws IllegalArgumentException {
+        if (fileName == null) {
+            return NOT_FOUND;
+        }
+        if (isSystemWindows()) {
+            // Special handling for NTFS ADS: Don't accept colon in the file name.
+            final int offset = fileName.indexOf(':', getAdsCriticalOffset(fileName));
+            if (offset != -1) {
+                throw new IllegalArgumentException("NTFS ADS separator (':') in file name is forbidden.");
+            }
+        }
+        final int extensionPos = fileName.lastIndexOf(EXTENSION_SEPARATOR);
+        final int lastSeparator = indexOfLastSeparator(fileName);
+        return lastSeparator > extensionPos ? NOT_FOUND : extensionPos;
+    }
+
+    private static int indexOfLastSeparator(final String fileName) {
+        if (fileName == null) {
+            return NOT_FOUND;
+        }
+        final int lastUnixPos = fileName.lastIndexOf(UNIX_NAME_SEPARATOR);
+        final int lastWindowsPos = fileName.lastIndexOf(WINDOWS_NAME_SEPARATOR);
+        return Math.max(lastUnixPos, lastWindowsPos);
+    }
+
+    private static boolean isSystemWindows() {
+        return SYSTEM_NAME_SEPARATOR == WINDOWS_NAME_SEPARATOR;
+    }
+
+    /**
+     * Special handling for NTFS ADS: Don't accept colon in the file name.
+     *
+     * @param fileName a file name
+     * @return ADS offsets.
+     */
+    private static int getAdsCriticalOffset(final String fileName) {
+        // Step 1: Remove leading path segments.
+        final int offset1 = fileName.lastIndexOf(SYSTEM_NAME_SEPARATOR);
+        final int offset2 = fileName.lastIndexOf(OTHER_SEPARATOR);
+        if (offset1 == -1) {
+            if (offset2 == -1) {
+                return 0;
+            }
+            return offset2 + 1;
+        }
+        if (offset2 == -1) {
+            return offset1 + 1;
+        }
+        return Math.max(offset1, offset2) + 1;
+    }
+
+    private static char flipSeparator(final char ch) {
+        if (ch == UNIX_NAME_SEPARATOR) {
+            return WINDOWS_NAME_SEPARATOR;
+        }
+        if (ch == WINDOWS_NAME_SEPARATOR) {
+            return UNIX_NAME_SEPARATOR;
+        }
+        throw new IllegalArgumentException(String.valueOf(ch));
     }
 }
