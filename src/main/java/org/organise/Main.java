b77b92ae8bdf3466cad3f05bcf21af672c91b798
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-06-11 14:07:52 +0200

    Testing the code in the main class

--- src/main/java/org/organise/Main.java
+++ src/main/java/org/organise/Main.java
@@ -1,7 +1,48 @@
 package org.organise;
 
+import org.organise.classifier.ClassifyByFileExtension;
+import org.organise.classifier.lookup.ExtensionLookUp;
+import org.organise.configuration.JSONConfigurationLoader;
+import org.organise.extractor.FileExtensionExtractor;
+import org.organise.mover.FileMover;
+import org.organise.organiser.FileOrganiser;
+import org.organise.scanner.DirectoryScanner;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Path configurationPath = Path.of("/home/rega/Tutorial/config/Config.json");
+        Path sourcePath = Path.of("/run/media/rega/Emperor/UNSTRUCTURED");
+
+        JSONConfigurationLoader jsonConfigurationLoader = JSONConfigurationLoader.getInstance(configurationPath);
+        ExtensionLookUp extensionLookUp = new ExtensionLookUp(jsonConfigurationLoader.getJsonConfigurationObject());
+
+        FileExtensionExtractor fileExtensionExtractor = new FileExtensionExtractor();
+
+        ClassifyByFileExtension classifyByFileExtension = new ClassifyByFileExtension(
+                extensionLookUp,
+                fileExtensionExtractor
+        );
+
+        FileMover fileMover = new FileMover();
+
+        DirectoryScanner directoryScanner = new DirectoryScanner(sourcePath);
+        List<Path> pathList = directoryScanner.scan();
+
+        FileOrganiser fileOrganiser = new FileOrganiser(
+                classifyByFileExtension,
+                fileMover,
+                directoryScanner
+        );
+
+        Path destinationDirectory = Paths.get("/run/media/rega/Emperor/Destination");
+
+        fileOrganiser.organise(destinationDirectory);
+
     }
 }
