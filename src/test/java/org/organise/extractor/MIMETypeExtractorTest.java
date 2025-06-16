a3f56cb676e80ba32de69eb208a7edf83aa1da90
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-06-16 16:04:47 +0200

    Renaming from FileTypeExtractorTest

--- src/test/java/org/organise/extractor/MIMETypeExtractorTest.java
+++ src/test/java/org/organise/extractor/MIMETypeExtractorTest.java
@@ -6,11 +6,11 @@ import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class FileTypeExtractorTest {
+class MIMETypeExtractorTest {
 
     @Test
     void testExtract_ValidFile() {
-        FileTypeExtractor extractor = new FileTypeExtractor();
+        MIMETypeExtractor extractor = new MIMETypeExtractor();
         Path file = Path.of("/home/rega/IdeaProjects/Organizer/src/main/java/org/organise/Main.java");
         String fileType = extractor.extract(file);
         assertEquals("text/plain", fileType); // Assuming "example.txt" resolves to "text/plain"
@@ -18,7 +18,7 @@ class FileTypeExtractorTest {
 
     @Test
     void testExtract_UnknownType() {
-        FileTypeExtractor extractor = new FileTypeExtractor();
+        MIMETypeExtractor extractor = new MIMETypeExtractor();
         Path file = Path.of("/home/rega/Tutorial/Java/FileOrganizer/Unknown.unknown");
         String fileType = extractor.extract(file);
         assertEquals("Unknown Type", fileType);
@@ -26,7 +26,7 @@ class FileTypeExtractorTest {
 
     @Test
     void testExtract_InvalidFile() {
-        FileTypeExtractor extractor = new FileTypeExtractor();
+        MIMETypeExtractor extractor = new MIMETypeExtractor();
         Path file = Path.of("");
         String fileType = extractor.extract(file);
         assertEquals("Unknown Type", fileType);
