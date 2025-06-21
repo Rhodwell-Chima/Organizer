6096f330d5683ff58dbe996ba276ba1cb400bf82
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-06-21 17:57:18 +0200

    Changing the logic to fit with the abstract class implementation "DateTimeExtractor"

--- src/main/java/org/organise/extractor/CreationDateExtractor.java
+++ src/main/java/org/organise/extractor/CreationDateExtractor.java
@@ -7,28 +7,15 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-public class CreationDateExtractor implements DataExtractor<LocalDateTime> {
+public class CreationDateExtractor extends DateTimeExtractor {
+
     @Override
-    public LocalDateTime extract(Path file) {
-        this.validateFile(file);
-        try {
-            return getCreationDate(file);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    protected LocalDateTime getDateTime(Path file) throws IOException {
+        return getCreationDate(file);
     }
 
     private LocalDateTime getCreationDate(Path file) throws IOException {
         BasicFileAttributes attributes = Files.readAttributes(file, BasicFileAttributes.class);
         return LocalDateTime.ofInstant(attributes.creationTime().toInstant(), ZoneId.systemDefault());
-    }
-
-    private void validateFile(Path file) {
-        if (file == null || file.getFileName() == null) {
-            throw new IllegalArgumentException("File path cannot be null or empty.");
-        }
-        if (!Files.isReadable(file)) {
-            throw new IllegalArgumentException("File is not readable: " + file);
-        }
     }
 }
--- src/main/java/org/organise/extractor/LastModifiedDateTimeExtractor.java
+++ src/main/java/org/organise/extractor/LastModifiedDateTimeExtractor.java
@@ -7,28 +7,15 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-public class LastModifiedDateTimeExtractor implements DataExtractor<LocalDateTime> {
+public class LastModifiedDateTimeExtractor extends DateTimeExtractor {
+
     @Override
-    public LocalDateTime extract(Path file) {
-        this.validateFile(file);
-        try {
-            return getLastModifiedDateTime(file);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    protected LocalDateTime getDateTime(Path file) throws IOException {
+        return getLastModifiedDateTime(file);
     }
 
     private LocalDateTime getLastModifiedDateTime(Path file) throws IOException {
         BasicFileAttributes basicFileAttributes = Files.readAttributes(file, BasicFileAttributes.class);
         return LocalDateTime.ofInstant(basicFileAttributes.lastModifiedTime().toInstant(), ZoneId.systemDefault());
-    }
-
-    private void validateFile(Path file) {
-        if (file == null || file.getFileName() == null) {
-            throw new IllegalArgumentException("File path cannot be null or empty.");
-        }
-        if (!Files.isReadable(file)) {
-            throw new IllegalArgumentException("File is not readable: " + file);
-        }
     }
 }
