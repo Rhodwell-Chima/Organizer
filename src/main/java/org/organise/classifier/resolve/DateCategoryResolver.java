547673abe50807f5befc95489ac57cf38a6297c6
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-06-21 13:32:32 +0200

    Removing unused imports

--- src/main/java/org/organise/classifier/resolve/DateCategoryResolver.java
+++ src/main/java/org/organise/classifier/resolve/DateCategoryResolver.java
@@ -1,16 +1,10 @@
 // Language: java
 package org.organise.classifier.resolve;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class DateCategoryResolver implements CategoryResolver {
 
