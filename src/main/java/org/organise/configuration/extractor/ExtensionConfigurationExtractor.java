26dd7577f8944d370e85a1f94b12bde664acbe26
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-06-21 11:58:54 +0200

    Making the method extract, return Map<String, List<String>> and implementing logic for parsing jsonMap to a given data Type.

--- src/main/java/org/organise/configuration/extractor/ExtensionConfigurationExtractor.java
+++ src/main/java/org/organise/configuration/extractor/ExtensionConfigurationExtractor.java
@@ -1,24 +1,42 @@
 package org.organise.configuration.extractor;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-import java.util.Map;
+import java.util.*;
 
-public class ExtensionConfigurationExtractor implements ConfigurationExtractor {
+public class ExtensionConfigurationExtractor implements ConfigurationExtractor<String> {
     private final JsonObject jsonObject;
-    private final String OBJECT_KEY = "Extension_Rules";
+    private final String OBJECT_KEY;
 
-    public ExtensionConfigurationExtractor(JsonObject jsonObject) {
+    public ExtensionConfigurationExtractor(JsonObject jsonObject, String objectKey) {
         if (jsonObject == null) {
             throw new IllegalArgumentException("JSON object cannot be null");
         }
         this.jsonObject = jsonObject;
+        this.OBJECT_KEY = objectKey;
     }
 
     @Override
-    public Map<String, JsonElement> extract() {
-        return this.getJsonObject().asMap();
+    public Map<String, List<String>> extract() {
+        return parseExtensionRules(getJsonObject().asMap());
+    }
+
+
+    private Map<String, List<String>> parseExtensionRules(Map<String, JsonElement> jsonMap) {
+        Map<String, List<String>> extensionRules = new HashMap<>();
+        for (Map.Entry<String, JsonElement> entry : jsonMap.entrySet()) {
+            JsonArray jsonArray = entry.getValue().getAsJsonArray();
+            List<String> extensions = new ArrayList<>();
+            if (jsonArray != null) {
+                for (JsonElement element : jsonArray) {
+                    extensions.add(element.getAsString());
+                }
+            }
+            extensionRules.put(entry.getKey(), extensions);
+        }
+        return extensionRules;
     }
 
     private JsonObject getJsonObject() {
