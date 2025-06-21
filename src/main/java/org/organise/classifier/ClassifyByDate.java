4e7199753346149081561b4a1c238792279eacb3
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-06-21 18:17:19 +0200

    Adding the logic for classifying File by Date.

--- src/main/java/org/organise/classifier/ClassifyByDate.java
+++ src/main/java/org/organise/classifier/ClassifyByDate.java
@@ -1,14 +1,36 @@
 package org.organise.classifier;
 
+import org.organise.classifier.resolve.DateCategoryResolver;
+import org.organise.extractor.DateTimeExtractor;
+
 import java.nio.file.Path;
 
 public class ClassifyByDate implements ClassifyFile<String> {
+
+    private final DateCategoryResolver dateCategoryResolver;
+    private final DateTimeExtractor dateTimeExtractor;
+
+    public ClassifyByDate(
+            DateCategoryResolver dateCategoryResolver,
+            DateTimeExtractor dateTimeExtractor
+    ) {
+        if (dateCategoryResolver == null || dateTimeExtractor == null) {
+            throw new IllegalArgumentException("Dependencies cannot be null");
+        }
+        this.dateCategoryResolver = dateCategoryResolver;
+        this.dateTimeExtractor = dateTimeExtractor;
+    }
 
     @Override
     public String classify(Path file) {
         if (file == null) {
             throw new IllegalArgumentException("File path cannot be null");
         }
-        return null;
+        return categorizeFileByDateTime(file);
+    }
+
+    private String categorizeFileByDateTime(Path file) {
+        String extractedDateTime = dateTimeExtractor.extract(file).toString();
+        return dateCategoryResolver.lookup(extractedDateTime);
     }
 }
