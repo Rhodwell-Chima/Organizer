90ace147fd85df34f44c3e89cf9f3375576bf1b1
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-06-21 14:51:54 +0200

    Adding error detection logic in classify

--- src/main/java/org/organise/classifier/ClassifyByDate.java
+++ src/main/java/org/organise/classifier/ClassifyByDate.java
@@ -6,6 +6,9 @@ public class ClassifyByDate implements ClassifyFile<String> {
 
     @Override
     public String classify(Path file) {
+        if (file == null) {
+            throw new IllegalArgumentException("File path cannot be null");
+        }
         return null;
     }
 }
