d591d29fb2438c1d774cedcd3dd3cc142402b1ce
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-06-23 13:41:33 +0200

    Changing the generic of the interface from <List<T>> to just <T>.

--- src/main/java/org/organise/configuration/extractor/ConfigurationExtractor.java
+++ src/main/java/org/organise/configuration/extractor/ConfigurationExtractor.java
@@ -1,8 +1,7 @@
 package org.organise.configuration.extractor;
 
-import java.util.List;
 import java.util.Map;
 
 public interface ConfigurationExtractor<T> {
-    Map<String, List<T>> extract();
+    Map<String, T> extract();
 }
