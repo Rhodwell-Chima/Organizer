eaed4ba6d390d3f8ad57f67ee1a6af404eab4efe
Rhodwell Chima <rhodwell.chima@cs.unza.zm>
2025-06-27 16:18:22 +0200

    Adding ClassifyByFileSize method and the Constructor

--- src/main/java/org/organise/classifier/ClassifyByFileSize.java
+++ src/main/java/org/organise/classifier/ClassifyByFileSize.java
@@ -1,10 +1,32 @@
 package org.organise.classifier;
 
+import org.organise.classifier.resolve.FileSizeCategoryResolver;
+import org.organise.extractor.FileSizeExtractor;
+
 import java.nio.file.Path;
 
 public class ClassifyByFileSize implements ClassifyFile<String> {
+    private final FileSizeCategoryResolver fileSizeCategoryResolver;
+    private final FileSizeExtractor fileSizeExtractor;
+
+    public ClassifyByFileSize(FileSizeCategoryResolver fileSizeCategoryResolver, FileSizeExtractor fileSizeExtractor) {
+        if (fileSizeCategoryResolver == null || fileSizeExtractor == null) {
+            throw new IllegalArgumentException("Dependencies cannot be null");
+        }
+        this.fileSizeCategoryResolver = fileSizeCategoryResolver;
+        this.fileSizeExtractor = fileSizeExtractor;
+    }
+
     @Override
     public String classify(Path file) {
-        return "";
+        if (file == null) {
+            throw new IllegalArgumentException("File path cannot be null");
+        }
+        return classifyFileBySize(file);
+    }
+
+    private String classifyFileBySize(Path file) {
+        long extractedFileSize = fileSizeExtractor.extract(file);
+        return fileSizeCategoryResolver.lookup(Long.toString(extractedFileSize));
     }
 }
